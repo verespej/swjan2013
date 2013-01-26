9e59ef5731e45bae2207fbcd7051f94c9c196e7e
Hakon Verespej <hakon@d-10-150-87-66.dhcp4.washington.edu>
2013-01-26 12:33:35 -0800

    Adding base RoR project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tunetether::Application.config.secret_token = '16ec73ad0746935eb13c459c6a968fe62b5c13d22e8f95bfc8c070e129b71f0ffe7c880e87dd92c7fed541beaaf7d403195d5fd08ad65988f4a9c6ef5c6226f4'
